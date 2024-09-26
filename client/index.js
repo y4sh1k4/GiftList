3869774da1a4e08cfdabd0c450897c1cbc4f5732
yashika <mehndirattayashika5@gmail.com>
2024-09-27 03:23:00 +0530

    first commit

--- client/index.js
+++ client/index.js
@@ -5,10 +5,14 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
+  const merkleTree = new MerkleTree(niceList);
+  // TODO: how do we prove to the server we're on the nice list?
+  const name="nolan"
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof,name
   });
 
   console.log({ gift });
